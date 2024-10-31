887b225eb72328e0cde2b5ecab92967477e955cf
MuhammadPro <MuhAbdulla@smail.pembinatrails.ca>
2024-10-31 13:51:26 -0500

    Add bot.sh script

---
+++ Bot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Hi,what your name?"
+read name
+echo " $name! thats a very cool name"
+sleep 3 #pause's the cript for 3 seconds
+echo " $name! what do you like?"
+read answer
+echo "oh, I like video games btw my name is Bot, what do you think?"
+read answer2
+echo "thank you for sharing your thoughts it was nice talking to you $name!"
